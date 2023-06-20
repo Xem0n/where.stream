44339719d2d43e291cf8b9be99ae8b08df21c5de
Xem0n <biedukiewicz87@gmail.com>
2023-06-20 18:35:15 +0200

    declare fetching data functions

---
+++ src/api/data.ts
@@ -0,0 +1,23 @@
+async function loadData() {
+  const services = await fetchServices();
+
+  // update localStorage 
+}
+
+async function fetchServices(): Promise<any> {
+  return {};
+}
+
+function getServices() {
+
+}
+
+function getCountries() {
+
+}
+
+export {
+  loadData,
+  getServices,
+  getCountries,
+};
