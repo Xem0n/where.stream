85b5fe355f4b4ddebcb55eb242f927a05bd3c946
Xem0n <biedukiewicz87@gmail.com>
2023-06-22 00:10:35 +0200

    fetch shows by title

--- src/components/search/index.tsx
+++ src/components/search/index.tsx
@@ -1,20 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ShowTypes from '../../types/ShowTypes';
+import Loading from '../Loading';
 import Form from './Form';
 import Results from './Results';
-// import styles from './App.module.css';
+
+const MIN_TITLE_LENGTH = 3;
 
 function Search() {
-  const getMovies = (title: string, country: string, type: ShowTypes) => {
-    console.log(title);
-    console.log(country);
-    console.log(type);
+  const [isSearching, setSearching] = useState(false);
+
+  const getMovies = async (title: string, country: string, type: ShowTypes) => {
+    setSearching(true);
+
+    if (title.length < MIN_TITLE_LENGTH) {
+      return;
+    }
+
+    const url = `https://streaming-availability.p.rapidapi.com/v2/search/title?title=${title}&country=${country}&show_type=${type}&output_language=en`;
+    const options = {
+        method: 'GET',
+        headers: {
+            'X-RapidAPI-Key': 'd8f31e989dmsh23bf5ea7462eb34p13be50jsn7e0152ef9e48',
+            'X-RapidAPI-Host': 'streaming-availability.p.rapidapi.com'
+        }
+    };
+
+    try {
+      const response = await fetch(url, options);
+      const data = await response.json();
+
+      console.log(data);
+    } catch (e: any) {
+      console.error(e);
+    }
+
+    setSearching(false);
   };
 
   return (
     <>
       <Form onSubmit={getMovies} />
-      <Results />
+      {isSearching ? 
+        <Loading /> : 
+        <Results />
+      }
     </>
   );
 }
