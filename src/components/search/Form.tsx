e9a356b0f01fe14cc8fa92b7bbc4cbb7501404cc
Xem0n <biedukiewicz87@gmail.com>
2023-06-21 23:51:51 +0200

    display actual list of countries and use ShowTypes enum

--- src/components/search/Form.tsx
+++ src/components/search/Form.tsx
@@ -1,14 +1,26 @@
 import React, { FormEvent, useState } from 'react';
+import { getCountries } from '../../api/data';
+import ShowTypes from '../../types/ShowTypes';
 import styles from './Form.module.css';
 
 interface FormProps {
-  onSubmit: (title: string, country: string, type: string) => void;
+  onSubmit: (title: string, country: string, type: ShowTypes) => void;
 }
 
 function Form(props: FormProps) {
+  const countries = getCountries();
+
+  const defaultCountry = countries.find(
+    country => navigator.language.toLowerCase().includes(country.toLowerCase())
+  ) ?? 'us';
+
+  const listCountries = countries.map(
+    country => <option key={country}>{country.toUpperCase()}</option>
+  );
+
   const [title, setTitle] = useState('');
-  const [country, setCountry] = useState('');
-  const [type, setType] = useState('');
+  const [type, setType] = useState(ShowTypes.All);
+  const [country, setCountry] = useState(defaultCountry);
 
   const onSubmit = (event: FormEvent) => {
     event.preventDefault();
@@ -29,19 +41,17 @@ function Form(props: FormProps) {
         <div className={styles.Selects}>
           <div className={styles.Select}>
             <select value={type}
-                    onChange={e => setType(e.target.value)}>
-              <option>All</option>
-              <option>Series</option>
-              <option>Movies</option>
+                    onChange={e => setType(Number(e.target.value) as ShowTypes)}>
+              <option value={ShowTypes.All}>All</option>
+              <option value={ShowTypes.Series}>Series</option>
+              <option value={ShowTypes.Movies}>Movies</option>
             </select>
           </div>
 
           <div className={styles.Select}>
-            <select value={country}
+            <select value={country.toUpperCase()}
                     onChange={e => setCountry(e.target.value)}>
-              <option>PL</option>
-              <option>US</option>
-              <option>DE</option>
+              {listCountries}
             </select>
           </div>
         </div>
