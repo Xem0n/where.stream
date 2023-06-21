da3234e9eb73e5428ce313994e098b80d3b75e67
Xem0n <biedukiewicz87@gmail.com>
2023-06-21 22:06:34 +0200

    add loading component

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,33 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { loadData } from './api/data';
 import styles from './App.module.css';
+import Loading from './components/Loading';
 
 interface AppProps {
   children: JSX.Element | JSX.Element[];
 }
 
 function App(props: AppProps) {
+  const [isLoading, setLoading] = useState(true);
+
+  useEffect(() => {
+    loadData().then(() => {
+      setLoading(false);
+    });
+
+    return () => {
+      setLoading(true);
+    };
+  }, []);
+
+  const children = isLoading ? <Loading /> : props.children;
+
   return (
     <div className={styles.App}>
       <header className={styles.Header}>
         <a className={styles.Link} href='/'>where.stream</a>
       </header>
-      {props.children}
+      {children}
     </div>
   );
 }
