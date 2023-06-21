125a182dbe037b482c311fee16e9c86d561e85cc
Xem0n <biedukiewicz87@gmail.com>
2023-06-21 23:51:34 +0200

    add ShowTypes enum

--- src/components/search/index.tsx
+++ src/components/search/index.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import ShowTypes from '../../types/ShowTypes';
 import Form from './Form';
 import Results from './Results';
 // import styles from './App.module.css';
 
 function Search() {
-  const getMovies = (title: string, country: string, type: string) => {
+  const getMovies = (title: string, country: string, type: ShowTypes) => {
     console.log(title);
     console.log(country);
     console.log(type);
---
+++ src/types/ShowTypes.ts
@@ -0,0 +1,7 @@
+enum ShowTypes {
+  All,
+  Series,
+  Movies
+}
+
+export default ShowTypes;
